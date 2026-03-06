f49558020093800e6ecf8b6ec2cb49124c61525d
apknation <atanasikafuka@gmail.com>
2026-03-06 16:09:27 +0300

    Fix Sum import with DBSum alias to resolve NameError

--- core/views.py
+++ core/views.py
@@ -12,7 +12,8 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, Count, Avg, Sum  # Fixed: Sum imported to resolve NameError
+from django.db.models import Q, Count, Avg, Sum
+from django.db.models import Sum as DBSum  # Alias to ensure no conflicts
 from django.urls import reverse_lazy, reverse
 from django.utils import timezone
 from django.views.generic import (
@@ -533,7 +534,7 @@ def admin_dashboard(request):
     active_enrollments = Enrollment.objects.count()
     
     # Fee information
-    total_fees_collected = Fee.objects.filter(is_paid=True).aggregate(total=Sum('amount'))['total'] or 0
+    total_fees_collected = Fee.objects.filter(is_paid=True).aggregate(total=DBSum('amount'))['total'] or 0
     pending_fees = Fee.objects.filter(is_paid=False).count()
     overdue_fees = Fee.objects.filter(is_paid=False, due_date__lt=timezone.now().date()).count()
     
@@ -619,8 +620,8 @@ class StudentDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
         context['fees'] = fees
         
         # Calculate fee statistics
-        total_fees = fees.aggregate(total=Sum('amount'))['total'] or 0
-        paid_fees = fees.filter(status='paid').aggregate(total=Sum('amount'))['total'] or 0
+        total_fees = fees.aggregate(total=DBSum('amount'))['total'] or 0
+        paid_fees = fees.filter(status='paid').aggregate(total=DBSum('amount'))['total'] or 0
         context['total_fees'] = total_fees
         context['paid_fees'] = paid_fees
         context['outstanding_fees'] = total_fees - paid_fees
