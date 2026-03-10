23cc1a112d466e8495d20307452b28b33f4b9696
apknation <atanasikafuka@gmail.com>
2026-03-10 18:51:20 +0300

    Update models

--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import User
 from django.utils.text import slugify
 
 
